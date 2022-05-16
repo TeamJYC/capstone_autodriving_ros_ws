e6946eb87efb268c7c0ff800a3f2457de8a15c42
seunggeon <tmdrjs9555@naver.com>
2022-05-16 18:46:03 +0900

    angular move first

--- src/willcar/scripts/cmd_vel.py
+++ src/willcar/scripts/cmd_vel.py
@@ -126,13 +126,31 @@ def subscribe_topic_message(data):
     linear_pwm_X = int(data.linear.x * 100) # 0.5m/s = 50 pwm [Hz]
     angle_pwm = int(data.angular.z*360/(2*PI))
 
+    if angle_pwm <= 180 and angle_pwm >= 10 :
+        angle_pwm = 50
+        car.Car_Right(angle_pwm, 0)
+        time.sleep(2)
+        car.Car_Stop()
+
+        print("Right angle_pwm")
+        print(angle_pwm)
+
+    if angle_pwm >= 190 and angle_pwm <= 360 :
+        angle_pwm = 50
+        car.Car_Left(0, angle_pwm)
+        time.sleep(2)
+        car.Car_Stop()
+
+        print("Left angle_pwm")
+        print(angle_pwm)
+
     if linear_pwm_X <= 30 and linear_pwm_X >= 10:
         linear_pwm_X = 30
         car.Car_Run(linear_pwm_X, linear_pwm_X)
         time.sleep(2)
         car.Car_Stop()
 
-        print("linear_pwm_X")
+        print("10 ~ 30 linear_pwm_X")
         print(linear_pwm_X)
 
     elif linear_pwm_X > 30 :
@@ -140,27 +158,11 @@ def subscribe_topic_message(data):
         time.sleep(2)
         car.Car_Stop()
 
-        print("linear_pwm_X")
+        print("30 ~ linear_pwm_X")
         print(linear_pwm_X)
         
 
-    if angle_pwm <= 180 and angle_pwm >= 30 :
-        angle_pwm = 50
-        car.Car_Right(angle_pwm, 0)
-        time.sleep(2)
-        car.Car_Stop()
 
-        print("angle_pwm")
-        print(angle_pwm)
-
-    if angle_pwm >= 210 and angle_pwm <= 360 :
-        angle_pwm = 50
-        car.Car_Left(0, angle_pwm)
-        time.sleep(2)
-        car.Car_Stop()
-
-        print("angle_pwm")
-        print(angle_pwm)
 
    
     
